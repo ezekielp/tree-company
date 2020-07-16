8d6dda5b022af29c71323ed2fb2cefd142fec1d2
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-07-16 17:28:12 -0400

    Create mutation for updating session[:cart]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :ensure_session_token
 
     def ensure_session_token
-        if !session[:session_token] || expired?
+        if !session[:session_token] || session_token_expired?
             session[:session_token] = SecureRandom.urlsafe_base64(16)
-            session[:session_cart] = []
+            session[:cart] = {}
         end
             
         set_session_expiration
     end
 
-    def expired?
+    def session_token_expired?
         session[:expires_at] > Time.current ? True : False
     end
 
