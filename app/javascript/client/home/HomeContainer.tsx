8fe6955dd6c2c5c43304b0de43a26fb6bdcb042a
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-07-16 16:31:44 -0400

    Create home container (#11)
    * Start writing code for HomeContainer component
    
    * add homepage and container
    
    * Add ProductThumbnailContainer file

--- app/javascript/client/home/HomeContainer.tsx
+++ app/javascript/client/home/HomeContainer.tsx
@@ -1,10 +1,44 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { useGetProductsForHomeContainerQuery, ProductInfoFragmentDoc } from '../graphqlTypes';
+import gql from 'graphql-tag';
+import { HomePage } from './HomePage';
+
+gql`
+    query GetProductsForHomeContainer {
+        products {
+            ...ProductInfo
+        }
+    }
+
+    ${ProductInfoFragmentDoc}
+`;
+
+gql`
+    fragment ProductInfo on Product {
+        id
+        name
+        size
+        material
+        description
+        styleNumber
+        counties {
+            id
+            name
+        }
+        imageUrl
+    }
+`;
 
 interface HomeContainerProps {}
 
 export const HomeContainer: FC<HomeContainerProps & RouteComponentProps> = () => {
+    const { data } = useGetProductsForHomeContainerQuery();
+    const products = data?.products;
+    
+    if (!products) return null;
+
     return (
-        <div>WELCOME TO THE TREE COMPANY!</div>
+        <HomePage products={products}></HomePage>
     )
 }
---
+++ app/javascript/client/product/thumbnail/ProductThumbnailContainer.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+
+
+
+
+
