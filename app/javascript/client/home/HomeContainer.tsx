f56b0c977525402a8a1180e26a0634b093519c55
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-07-16 13:03:01 -0400

    Start writing code for HomeContainer component

--- app/javascript/client/home/HomeContainer.tsx
+++ app/javascript/client/home/HomeContainer.tsx
@@ -1,5 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import gql from 'graphql-tag';
+
+gql`
+    query GetProductsForHomeContainer {
+        products {
+            id
+            name
+            size
+            material
+            description
+            style_number
+            counties
+            image_url
+        }
+    }
+`
 
 interface HomeContainerProps {}
 
