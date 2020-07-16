fbaa8ff1ba592811fd73f5e0ade5c8a937f7bb7b
Andrew Lee <leea2142@gmail.com>
2020-07-16 15:05:50 -0400

    Session cart (#9)
    * testing
    
    * changed to use Time methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    def ensure_session_token
+        if !session[:session_token] || expired?
+            session[:session_token] = SecureRandom.urlsafe_base64(16)
+            session[:session_cart] = []
+        end
+            
+        set_session_expiration
+    end
+
+    def expired?
+        session[:expires_at] > Time.current ? True : False
+    end
+
+    def set_session_expiration
+        session[:expires_at] = Time.current + 7.days
+    end
+
 end
