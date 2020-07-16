a8a70e0b1b3cc390083bf1ab7f51a760c2e80e1e
Andrew Lee <leea2142@gmail.com>
2020-07-16 10:58:00 -0400

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    def ensure_session_token
+        if !session[:session_token] || expired?
+            session[:session_token] = SecureRandom.urlsafe_base64(16)
+            session[:session_cart] = []
+        end
+            
+        renew_session_expiration
+    end
+
+    def expired?
+        session[:expires_at] > Time.now ? True : False
+    end
+
+    def renew_session_expiration
+        days = 24*60*60
+        session[:expires_at] = Time.now + 7*days
+    end
+
 end
