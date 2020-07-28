b2301c459bd5d11bb3712ed3c524c9c9728ba11f
Andrew Lee <leea2142@gmail.com>
2020-07-28 10:23:17 -0400

    add buttons and description

--- app/javascript/client/product/modals/ProductModal.tsx
+++ app/javascript/client/product/modals/ProductModal.tsx
@@ -2,6 +2,31 @@ import React, { FC, useContext } from 'react';
 import styled from 'styled-components';
 import { ModalContext } from '../../home/HomePage';
 
+const AddToCartButton = styled.button`
+
+`
+
+const CloseModalButton = styled.button`
+    width: 100px;
+    height: 50px;
+    cursor: pointer;
+`
+
+const InputQuantity = styled.input`
+
+`
+
+const ProductInformation = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const ProductImageContainer = styled.img`
+    object-fit: cover;
+    width: 290px;
+    height: 390px;
+`;
+
 const ProductModalContainer = styled.div`
     display: flex;
     flex-direction: column;
@@ -12,16 +37,10 @@ const ProductModalContainer = styled.div`
     padding: 10px;
 `;
 
-const CloseModalButton = styled.button`
-    width: 100px;
-    height: 50px;
-    cursor: pointer;
+const ProductName = styled.div`
+    font-size: 18px;
+    font-weight: bold;
 `
-const ProductImageContainer = styled.img`
-    object-fit: cover;
-    width: 290px;
-    height: 390px;
-`;
 
 interface ProductModalProps {}
 
@@ -35,10 +54,12 @@ const ProductModal: FC<ProductModalProps> = () => {
 
     return (
         <ProductModalContainer onClick={(e) => e.stopPropagation()}>
-            <div>{selectedProduct.name}</div>
-            <div>{selectedProduct.material}</div>
-            <div>{selectedProduct.description}</div>
-            <div>{selectedProduct.size}</div>
+            <ProductName>{selectedProduct.name}</ProductName>
+            <ProductInformation>
+                <span>Material: {selectedProduct.material}</span>
+                <span>Size: {selectedProduct.size}</span>
+                {selectedProduct.description != "" && <span>Description: {selectedProduct.description}</span>}
+            </ProductInformation>
             <ProductImageContainer src={selectedProduct.imageUrl} />
             <CloseModalButton onClick={()=>closeModal()}>Close</CloseModalButton>
         </ProductModalContainer>
