6aad8aa930deb847b053f2f9a00d6f6c641f5472
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-08-04 21:19:01 -0400

    Copy tweak

--- app/javascript/client/error/ErrorPage.tsx
+++ app/javascript/client/error/ErrorPage.tsx
@@ -17,7 +17,7 @@ export const ErrorPage: FC<ErrorPageProps> = () => {
                 Oops! Something went wrong on our end. Our apologies.
             </ErrorMessage>
             <ErrorMessage>
-                Please contact Jim directly by email at sales@thetreecompany.com or by phone at 410-788-7277 to place your order. Or, click here to download a printable order form that you can fax or mail in to place your order (you can find the relevant contact details on the form).
+                Please contact Jim directly by phone at 410-788-7277 or by email at sales@thetreecompany.com to place your order.
             </ErrorMessage>
             <ErrorMessage>
                 Thanks very much for your business!
