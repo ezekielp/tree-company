4230aeb3b193af6e13d02328f9d63c6b4e65e661
Andrew Lee <leea2142@gmail.com>
2020-08-12 15:04:24 -0400

    add forest_conservation to seed file and change validation on categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,7 +8,7 @@
 #  updated_at :datetime         not null
 #
 class Category < ApplicationRecord
-    validates :name, inclusion: { in: ["wetland_stream_buffer"] }
+    validates :name, inclusion: { in: ["wetland_stream_buffer", "forest_conservation"] }
 
     has_many :product_categories
     has_many :products, through: :product_categories
